776082bcdbfa5089ac1bdecb16ed6ed10d10d625
genuinelycool <resurrected07311@gmail.com>
2023-07-05 00:13:56 +0545

    remove comments, code cleanup

--- routes/api.php
+++ routes/api.php
@@ -16,24 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/products', function(){
-//     // return 'products';
-//     // return Product::all();
-// });
-
 // Route::get('/products', [ProductController::class, 'index']);
 // Route::post('/products', [ProductController::class, 'store']);
 
 Route::resource('products', ProductController::class);
-
-// Route::post('/products', function(){
-//     return Product::create([
-//         'name' => "Product One",
-//         'slug' => "product-one",
-//         'description' => 'This is product one',
-//         'price' => '99.99'
-//     ]);
-// });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
